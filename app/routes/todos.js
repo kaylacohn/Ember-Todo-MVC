63ef7b91aa31cc6f669dda46702e2a54cebb1aa2
Kayla Cohn <kayla.s.cohn@gmail.com>
2015-12-11 22:56:40 -0500

    Add mirage to fake a database

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -2,16 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    let todos = [
-      {
-        title: "Learn Ember",
-        complete: false,
-      },
-      {
-        title: "Solve World Hunder",
-        complete: false,
-      }
-    ];
-    return todos
-  }
+    return this.store.findAll('todo');
 });
